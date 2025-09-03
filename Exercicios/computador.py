ea01793e90b9f7bf73a19adf8fe8b7d81ccfecdd
Arthur Guilherme Jardim Soarres <arthursoaresj09@gmail.com>
2025-09-03 09:15:11 -0300

    Update computador.py

--- Exercicios/computador.py
+++ Exercicios/computador.py
@@ -1,16 +1,22 @@
 import random
+resp = 'S'
 print('=-'*20)
 print ('           COMPUTADOR PENSANTE             ')
 print('=-'*20)
 print ('Tente adivinhar o número que eu pensei entre 0 e 5')
-jogar=input('Deseja jogar?[S/N] ').upper()
+jogar=input('Deseja jogar?[S/N] ').strip().upper()[0]
 if jogar == 'S' or jogar == 'SIM':
-    num=random.randint(0,5)
-    print('Já pensei')
-    resposta=int(input('Qual o número? '))
-    if resposta == num:
-        print ('Parabéns! você acertou!')
-    else:
-        print('Você errou, eu pensei no número {}'.format(num))
+    while resp != 'N':
+        numero=random.randint(0,5)
+        num=random.randint(0,5)
+        print('Já pensei')
+        resposta=int(input('Qual o número? '))
+        if resposta == num:
+            print ('Parabéns! você acertou!')
+        else:
+            print('Você errou, eu pensei no número {}'.format(num))
+        resp=input('Deseja jogar novamente?[S/N] ').strip().upper()[0]
+        if resp == 'N':
+            print('Tudo bem!')
 else:
     print('Tudo bem!')
